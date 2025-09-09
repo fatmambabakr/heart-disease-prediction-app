5dfdaa282f10f9ec8db6706fef22e0a1fbd09f8c
fatma mohamed babakr <fatmambabakr@gmail.com>
2025-09-09 15:24:50 -0800

    Update heart disease app.py

--- heart disease app.py
+++ heart disease app.py
@@ -2,14 +2,14 @@ import streamlit as st
 import joblib
 import pandas as pd
 
-# ---------- Load the trained pipeline ----------
+
 model = joblib.load("heart_disease_modelv2 (1).pkl")  
 
-# ---------- Streamlit Title ----------
+
 st.title("💓 Heart Disease Prediction App")
 st.write("Please enter your medical data to generate a prediction")
 
-# ---------- User Inputs ----------
+
 age = st.number_input("Age", 18, 100, 30)
 sex = st.selectbox("Gender", ["Female", "Male"])
 sex_input = 0 if sex == "Female" else 1
@@ -23,9 +23,9 @@ exang = st.selectbox("Exercise Induced Angina", [0, 1])
 oldpeak = st.number_input("ST depression induced by exercise", 0.0, 10.0, 1.0)
 slope = st.selectbox("Slope of the peak exercise ST segment", [0, 1, 2])
 ca = st.selectbox("Number of major vessels colored by fluoroscopy", [0, 1, 2, 3])
-thal = st.selectbox("Thalassemia", [3, 6, 7])  # خليتيه زي الموديل
+thal = st.selectbox("Thalassemia", [3, 6, 7])  
 
-# ---------- Prepare features in the same order as trained pipeline ----------
+
 features = pd.DataFrame([[
     age, trestbps, chol, thalach, oldpeak,
     1 if ca == 1 else 0,
@@ -38,7 +38,7 @@ features = pd.DataFrame([[
     'ca_1.0', 'ca_2.0', 'ca_3.0', 'thal_6.0', 'thal_7.0'
 ])
 
-# ---------- Prediction ----------
+
 if st.button("Predict"):
     pred = model.predict(features)[0]
    
@@ -55,5 +55,4 @@ if st.button("Predict"):
         st.error("Very Severe Heart Disease 🚨🔥")
     else:
         st.write("Unknown Class")
-
 
